79c65922b9df08c87d1e2d79f2389fd88be30986
Konstantinos Loizas <konsloiz@gmail.com>
2021-03-18 15:24:10 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Mesoropi! I miss you! So much! So so. Mesoropi!!
         </p>
         <a
-          className="App-link"
+          className="Mesoropi-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
